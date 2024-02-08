f65ba2932d5616de8593073a4f4b2a661833977e
Galasso <galassoworkstation@gmail.com>
2024-02-08 13:16:04 -0300

    Create the Seats with unique names, such as A1,B4,D5, etc.

--- AirplaneManagement/src/dev/galasso/matheus/application/core/entities/Airplane.java
+++ AirplaneManagement/src/dev/galasso/matheus/application/core/entities/Airplane.java
@@ -3,12 +3,32 @@ package dev.galasso.matheus.application.core.entities;
 public class Airplane {
     private Seat[] allSeats;
     private int sections,chairsPerSection,linesPerSection;
+    private double framesOnPlane;
 
     public Airplane(int sections, int chairsPerSection, int linesPerSection) {
         this.sections = sections;
         this.chairsPerSection = chairsPerSection;
         this.linesPerSection = linesPerSection;
         final int CAPACITY = (this.sections * this.chairsPerSection * this.linesPerSection);
+        framesOnPlane = CAPACITY/(Math.pow(this.sections,this.chairsPerSection));
         allSeats = new Seat[CAPACITY];
+        createAllSeats();
+    }
+
+    private void createAllSeats(){
+        int counter = 0;
+        char frameCounter = 'A';
+        for (int i = 0; i < framesOnPlane; i++) {
+            for (int j = 0; j < Math.pow(this.sections,this.chairsPerSection); j++) {
+                allSeats[counter] = new Seat(frameCounter,j+1);
+                counter++;
+            }frameCounter++;
+        }
+    }
+    public void printSeats(){
+        for (Seat seat : allSeats) {
+            System.out.println(seat);
+        }
+
     }
 }
--- AirplaneManagement/src/dev/galasso/matheus/application/test/Main.java
+++ AirplaneManagement/src/dev/galasso/matheus/application/test/Main.java
@@ -1,10 +1,12 @@
 package dev.galasso.matheus.application.test;
 
+import dev.galasso.matheus.application.core.entities.Airplane;
 import dev.galasso.matheus.application.core.entities.Seat;
 
 public class Main {
     public static void main(String[] args) {
-        Seat seat = new Seat('A',1);
-        System.out.println(seat);
+        Airplane plane1 = new Airplane(2,4,20);
+        plane1.printSeats();
+
     }
 }
