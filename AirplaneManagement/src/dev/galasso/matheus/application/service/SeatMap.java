1a30f98d9538ae04d98abc40bcd150cb9fb26475
Galasso <galassoworkstation@gmail.com>
2024-02-12 18:41:19 -0300

    Created the printMap method. And saves the MapSeat in a multidimensional String Array

--- AirplaneManagement/src/dev/galasso/matheus/application/service/SeatMap.java
+++ AirplaneManagement/src/dev/galasso/matheus/application/service/SeatMap.java
@@ -3,22 +3,41 @@ package dev.galasso.matheus.application.service;
 import dev.galasso.matheus.application.core.entities.Seat;
 
 public class SeatMap {
+    private String[][] seatMap;
+    private int sectionBreak;
+
     public SeatMap() {
     }
 
-    public void createSeatMap(Seat[] allSeats, double framesOnPlane, int sectionsOnPlane, int rowsPerSection){
+    public void createSeatMap(Seat[] allSeats, double framesOnPlane, int sectionsOnPlane, int chairsPerSection) {
+        seatMap = new String[(int) framesOnPlane][chairsPerSection * sectionsOnPlane];
+        sectionBreak = chairsPerSection;
         int x = 0;
         int spaceBTWNSeats = 5;
         String formatSpecifier = "|  %-" + spaceBTWNSeats + "s  ";
         for (int i = 0; i < framesOnPlane; i++) {
             for (int j = 0; j < sectionsOnPlane; j++) {
-                for (int k = 0; k < rowsPerSection; k++) {
-                    System.out.printf(formatSpecifier, allSeats[x]);
+                for (int k = 0; k < chairsPerSection; k++) {
+                    int index = (j * chairsPerSection) + k;
+                    seatMap[i][index] = String.format(formatSpecifier, allSeats[x]);
                     x++;
                 }
-                System.out.print("|         ");
             }
-            System.out.println(" ");
+        }
+    }
+
+    public void printSeatMap() {
+        int x = 0;
+        for (String[] strings : seatMap) {
+            for (String string : strings) {
+                System.out.print(string);
+                x++;
+                if (x == sectionBreak) {
+                    System.out.print("|        ");
+                    x = 0;
+                }
+            }
+            System.out.println();
         }
     }
 }
